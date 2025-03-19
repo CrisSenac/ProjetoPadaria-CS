46aecf15d766ed9f1ef503dfc2365d0ca4d64285
CRISLENE DOS SANTOS SILVA <crislene.ssilva1@senacsp.edu.br>
2025-03-19 11:51:10 -0300

    Projeto Padaria

---
+++ Padaria/frmSplash.Designer.cs
@@ -0,0 +1,109 @@
+﻿namespace Padaria
+{
+    partial class frmSplash
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmSplash));
+            this.pcbLogo = new System.Windows.Forms.PictureBox();
+            this.lblNumero = new System.Windows.Forms.Label();
+            this.lblPorc = new System.Windows.Forms.Label();
+            this.pgbLogo = new System.Windows.Forms.ProgressBar();
+            ((System.ComponentModel.ISupportInitialize)(this.pcbLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pcbLogo
+            // 
+            this.pcbLogo.Image = ((System.Drawing.Image)(resources.GetObject("pcbLogo.Image")));
+            this.pcbLogo.Location = new System.Drawing.Point(227, 114);
+            this.pcbLogo.Name = "pcbLogo";
+            this.pcbLogo.Size = new System.Drawing.Size(275, 235);
+            this.pcbLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pcbLogo.TabIndex = 0;
+            this.pcbLogo.TabStop = false;
+            // 
+            // lblNumero
+            // 
+            this.lblNumero.AutoSize = true;
+            this.lblNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumero.Location = new System.Drawing.Point(342, 380);
+            this.lblNumero.Name = "lblNumero";
+            this.lblNumero.Size = new System.Drawing.Size(18, 20);
+            this.lblNumero.TabIndex = 1;
+            this.lblNumero.Text = "0";
+            this.lblNumero.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // lblPorc
+            // 
+            this.lblPorc.AutoSize = true;
+            this.lblPorc.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPorc.Location = new System.Drawing.Point(366, 380);
+            this.lblPorc.Name = "lblPorc";
+            this.lblPorc.Size = new System.Drawing.Size(23, 20);
+            this.lblPorc.TabIndex = 2;
+            this.lblPorc.Text = "%";
+            this.lblPorc.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // pgbLogo
+            // 
+            this.pgbLogo.Location = new System.Drawing.Point(272, 403);
+            this.pgbLogo.Name = "pgbLogo";
+            this.pgbLogo.Size = new System.Drawing.Size(197, 26);
+            this.pgbLogo.Style = System.Windows.Forms.ProgressBarStyle.Marquee;
+            this.pgbLogo.TabIndex = 3;
+            this.pgbLogo.Value = 50;
+            this.pgbLogo.Click += new System.EventHandler(this.progressBar1_Click);
+            // 
+            // frmSplash
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(784, 561);
+            this.Controls.Add(this.pgbLogo);
+            this.Controls.Add(this.lblPorc);
+            this.Controls.Add(this.lblNumero);
+            this.Controls.Add(this.pcbLogo);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmSplash";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Padaria - Splash";
+            ((System.ComponentModel.ISupportInitialize)(this.pcbLogo)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pcbLogo;
+        private System.Windows.Forms.Label lblNumero;
+        private System.Windows.Forms.Label lblPorc;
+        private System.Windows.Forms.ProgressBar pgbLogo;
+    }
+}
