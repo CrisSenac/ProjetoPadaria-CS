984cb2985ff6f2eeeb3993d2a6f4a0911bb762c6
CRISLENE DOS SANTOS SILVA <crislene.ssilva1@senacsp.edu.br>
2025-03-21 11:54:14 -0300

    calculadora

---
+++ Padaria/frmCalculadora.Designer.cs
@@ -0,0 +1,244 @@
+﻿namespace Padaria
+{
+    partial class frmCalculadora
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmCalculadora));
+            this.lblNumeroUm = new System.Windows.Forms.Label();
+            this.txtUm = new System.Windows.Forms.TextBox();
+            this.txtResposta = new System.Windows.Forms.TextBox();
+            this.lblResposta = new System.Windows.Forms.Label();
+            this.txtDois = new System.Windows.Forms.TextBox();
+            this.lblNumeroDois = new System.Windows.Forms.Label();
+            this.lblOperacoes = new System.Windows.Forms.Label();
+            this.rdbSomar = new System.Windows.Forms.RadioButton();
+            this.rdbDividir = new System.Windows.Forms.RadioButton();
+            this.rdbMultiplicar = new System.Windows.Forms.RadioButton();
+            this.rdbSubtrair = new System.Windows.Forms.RadioButton();
+            this.btnCalcular = new System.Windows.Forms.Button();
+            this.btnLimpar = new System.Windows.Forms.Button();
+            this.btnSair = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // lblNumeroUm
+            // 
+            this.lblNumeroUm.AutoSize = true;
+            this.lblNumeroUm.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumeroUm.Location = new System.Drawing.Point(34, 53);
+            this.lblNumeroUm.Name = "lblNumeroUm";
+            this.lblNumeroUm.Size = new System.Drawing.Size(78, 20);
+            this.lblNumeroUm.TabIndex = 0;
+            this.lblNumeroUm.Text = "Número 1";
+            // 
+            // txtUm
+            // 
+            this.txtUm.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtUm.Location = new System.Drawing.Point(38, 76);
+            this.txtUm.Name = "txtUm";
+            this.txtUm.Size = new System.Drawing.Size(100, 26);
+            this.txtUm.TabIndex = 0;
+            // 
+            // txtResposta
+            // 
+            this.txtResposta.Enabled = false;
+            this.txtResposta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtResposta.Location = new System.Drawing.Point(38, 234);
+            this.txtResposta.Name = "txtResposta";
+            this.txtResposta.Size = new System.Drawing.Size(100, 26);
+            this.txtResposta.TabIndex = 9;
+            // 
+            // lblResposta
+            // 
+            this.lblResposta.AutoSize = true;
+            this.lblResposta.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResposta.Location = new System.Drawing.Point(34, 211);
+            this.lblResposta.Name = "lblResposta";
+            this.lblResposta.Size = new System.Drawing.Size(78, 20);
+            this.lblResposta.TabIndex = 2;
+            this.lblResposta.Text = "Resposta";
+            // 
+            // txtDois
+            // 
+            this.txtDois.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtDois.Location = new System.Drawing.Point(38, 157);
+            this.txtDois.Name = "txtDois";
+            this.txtDois.Size = new System.Drawing.Size(100, 26);
+            this.txtDois.TabIndex = 1;
+            // 
+            // lblNumeroDois
+            // 
+            this.lblNumeroDois.AutoSize = true;
+            this.lblNumeroDois.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblNumeroDois.Location = new System.Drawing.Point(34, 134);
+            this.lblNumeroDois.Name = "lblNumeroDois";
+            this.lblNumeroDois.Size = new System.Drawing.Size(78, 20);
+            this.lblNumeroDois.TabIndex = 4;
+            this.lblNumeroDois.Text = "Número 2";
+            // 
+            // lblOperacoes
+            // 
+            this.lblOperacoes.AutoSize = true;
+            this.lblOperacoes.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblOperacoes.Location = new System.Drawing.Point(258, 53);
+            this.lblOperacoes.Name = "lblOperacoes";
+            this.lblOperacoes.Size = new System.Drawing.Size(87, 20);
+            this.lblOperacoes.TabIndex = 6;
+            this.lblOperacoes.Text = "Operações";
+            // 
+            // rdbSomar
+            // 
+            this.rdbSomar.AutoSize = true;
+            this.rdbSomar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbSomar.Location = new System.Drawing.Point(271, 92);
+            this.rdbSomar.Name = "rdbSomar";
+            this.rdbSomar.Size = new System.Drawing.Size(74, 24);
+            this.rdbSomar.TabIndex = 2;
+            this.rdbSomar.TabStop = true;
+            this.rdbSomar.Text = "Somar";
+            this.rdbSomar.UseVisualStyleBackColor = true;
+            // 
+            // rdbDividir
+            // 
+            this.rdbDividir.AutoSize = true;
+            this.rdbDividir.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbDividir.Location = new System.Drawing.Point(271, 138);
+            this.rdbDividir.Name = "rdbDividir";
+            this.rdbDividir.Size = new System.Drawing.Size(69, 24);
+            this.rdbDividir.TabIndex = 3;
+            this.rdbDividir.TabStop = true;
+            this.rdbDividir.Text = "Dividir";
+            this.rdbDividir.UseVisualStyleBackColor = true;
+            // 
+            // rdbMultiplicar
+            // 
+            this.rdbMultiplicar.AutoSize = true;
+            this.rdbMultiplicar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbMultiplicar.Location = new System.Drawing.Point(271, 220);
+            this.rdbMultiplicar.Name = "rdbMultiplicar";
+            this.rdbMultiplicar.Size = new System.Drawing.Size(97, 24);
+            this.rdbMultiplicar.TabIndex = 5;
+            this.rdbMultiplicar.TabStop = true;
+            this.rdbMultiplicar.Text = "Multiplicar";
+            this.rdbMultiplicar.UseVisualStyleBackColor = true;
+            // 
+            // rdbSubtrair
+            // 
+            this.rdbSubtrair.AutoSize = true;
+            this.rdbSubtrair.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.rdbSubtrair.Location = new System.Drawing.Point(271, 179);
+            this.rdbSubtrair.Name = "rdbSubtrair";
+            this.rdbSubtrair.Size = new System.Drawing.Size(83, 24);
+            this.rdbSubtrair.TabIndex = 4;
+            this.rdbSubtrair.TabStop = true;
+            this.rdbSubtrair.Text = "Subtrair";
+            this.rdbSubtrair.UseVisualStyleBackColor = true;
+            // 
+            // btnCalcular
+            // 
+            this.btnCalcular.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnCalcular.Image = ((System.Drawing.Image)(resources.GetObject("btnCalcular.Image")));
+            this.btnCalcular.Location = new System.Drawing.Point(531, 53);
+            this.btnCalcular.Name = "btnCalcular";
+            this.btnCalcular.Size = new System.Drawing.Size(145, 65);
+            this.btnCalcular.TabIndex = 6;
+            this.btnCalcular.Text = "&Calcular";
+            this.btnCalcular.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnCalcular.UseVisualStyleBackColor = true;
+            this.btnCalcular.Click += new System.EventHandler(this.btnCalcular_Click);
+            // 
+            // btnLimpar
+            // 
+            this.btnLimpar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLimpar.Image = ((System.Drawing.Image)(resources.GetObject("btnLimpar.Image")));
+            this.btnLimpar.Location = new System.Drawing.Point(531, 138);
+            this.btnLimpar.Name = "btnLimpar";
+            this.btnLimpar.Size = new System.Drawing.Size(145, 65);
+            this.btnLimpar.TabIndex = 7;
+            this.btnLimpar.Text = "&Limpar";
+            this.btnLimpar.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnLimpar.UseVisualStyleBackColor = true;
+            // 
+            // btnSair
+            // 
+            this.btnSair.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSair.Image = ((System.Drawing.Image)(resources.GetObject("btnSair.Image")));
+            this.btnSair.Location = new System.Drawing.Point(531, 220);
+            this.btnSair.Name = "btnSair";
+            this.btnSair.Size = new System.Drawing.Size(145, 65);
+            this.btnSair.TabIndex = 8;
+            this.btnSair.Text = "&Sair";
+            this.btnSair.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageBeforeText;
+            this.btnSair.UseVisualStyleBackColor = true;
+            // 
+            // frmCalculadora
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(718, 349);
+            this.Controls.Add(this.btnSair);
+            this.Controls.Add(this.btnLimpar);
+            this.Controls.Add(this.btnCalcular);
+            this.Controls.Add(this.rdbSubtrair);
+            this.Controls.Add(this.rdbMultiplicar);
+            this.Controls.Add(this.rdbDividir);
+            this.Controls.Add(this.rdbSomar);
+            this.Controls.Add(this.lblOperacoes);
+            this.Controls.Add(this.txtDois);
+            this.Controls.Add(this.lblNumeroDois);
+            this.Controls.Add(this.txtResposta);
+            this.Controls.Add(this.lblResposta);
+            this.Controls.Add(this.txtUm);
+            this.Controls.Add(this.lblNumeroUm);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "frmCalculadora";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Calculadora";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lblNumeroUm;
+        private System.Windows.Forms.TextBox txtUm;
+        private System.Windows.Forms.TextBox txtResposta;
+        private System.Windows.Forms.Label lblResposta;
+        private System.Windows.Forms.TextBox txtDois;
+        private System.Windows.Forms.Label lblNumeroDois;
+        private System.Windows.Forms.Label lblOperacoes;
+        private System.Windows.Forms.RadioButton rdbSomar;
+        private System.Windows.Forms.RadioButton rdbDividir;
+        private System.Windows.Forms.RadioButton rdbMultiplicar;
+        private System.Windows.Forms.RadioButton rdbSubtrair;
+        private System.Windows.Forms.Button btnCalcular;
+        private System.Windows.Forms.Button btnLimpar;
+        private System.Windows.Forms.Button btnSair;
+    }
+}
---
+++ Padaria/frmCalculadora.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Padaria
+{
+    public partial class frmCalculadora : Form
+    {
+        public frmCalculadora()
+        {
+            InitializeComponent();
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+            //declarando variaveis
+            //tipo de variavel e o nome da variavel
+
+            int valor1, valor4, resp;
+            double valor2;
+            float valor3;
+            bool flag;
+            string nome;
+            char sexo;
+
+            //inicializar as variaveis
+            valor1 = 10;
+            valor4 = 10;
+
+            resp = valor1 + valor4;
+        }
+    }
+}
